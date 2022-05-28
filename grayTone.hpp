5585444cbfb07a13754cfd24f0d2c7ed7581b969
kpalaciosm02 <kpalaciosm02@gmail.com>
2022-05-28 02:51:13 -0600

    Generating grayTones with arithmetics

---
+++ grayTone.hpp
@@ -0,0 +1,28 @@
+#include "color.hpp"
+#include <iostream>
+#ifndef GRAYTONE
+#define GRAYTONE
+
+class GrayTone{
+    private:
+
+    public:
+        GrayTone(){
+        }
+        Color rgbtoGray(Color color){
+            int average = (color.getR() + color.getG() + color.getB())/3;
+            int sixteenMultiplier;                                          //numero multiplo de 16 mas cercano al average para hacer el gris
+            if (average % 16 >= 8){
+                sixteenMultiplier = (average/16) + 1;
+            }
+            else{
+                sixteenMultiplier = average/16;
+            }
+            int grayCode = sixteenMultiplier * 16;
+            Color finalGray(grayCode,grayCode,grayCode,255);
+            finalGray.print();
+            return finalGray;
+        }
+};
+
+#endif
